8d61b7ef433655a2ee2f063650bc149738dc43ae
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-03-05 13:16:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DevContainer Sample
+
+This project provides a simple Spring Boot app written in Groovy and built with Gradle.
---
+++ src/main/groovy/de/schulung/spring/SimpleSpringAppApplication.groovy
@@ -0,0 +1,13 @@
+package de.schulung.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SimpleSpringAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SimpleSpringAppApplication, args)
+    }
+
+}
